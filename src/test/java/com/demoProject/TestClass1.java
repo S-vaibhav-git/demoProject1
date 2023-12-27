8c4d4d74493a4d908140fcc729dc5b7344665823
S-vaibhav-git <vaibhavsuradkar33@gmail.com>
2023-12-27 22:31:55 +0530

    browser stack related changes

--- src/test/java/com/demoProject/TestClass1.java
+++ src/test/java/com/demoProject/TestClass1.java
@@ -1,9 +1,13 @@
 package com.demoProject;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -13,17 +17,30 @@ public class TestClass1 {
 	public static WebDriver driver;
 
 	@BeforeMethod
-	public void launchDriver() {
+	public void launchDriver() throws MalformedURLException {
 
-		System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\Drivers\\chromedriver.exe");
-		driver=new ChromeDriver();
+//		System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\Drivers\\chromedriver.exe");
+//		driver=new ChromeDriver();
+		
+		// BrowserStack WebDriver instantiation
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability("browser", "Chrome");
+		caps.setCapability("browser_version", "latest");
+		caps.setCapability("os", "Windows");
+		caps.setCapability("os_version", "10");
+
+		WebDriver driver = new RemoteWebDriver(
+			    new URL("https://jaykhare_Of1lML:DGPyZ3sry9h3NAGUdJ9D@hub-cloud.browserstack.com/wd/hub"),
+			    caps
+			);
+		
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().fullscreen();
 	}
 
 	@Test
 	public void Test1() {
-		driver.navigate().to("https://www.automationtalks.com");
+		driver.navigate().to("https://www.bstackdemo.com");
 		System.out.println("Page 1 title is "+driver.getTitle());
 	}
 
@@ -41,6 +58,7 @@ public class TestClass1 {
 	
 	@AfterMethod
 	public void quite() {
-		driver.quit();
+		//driver.quit();
+		//driver.close();
 	}
 }
